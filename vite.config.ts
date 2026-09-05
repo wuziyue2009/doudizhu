100116bf4b8f19912787237a6fc803f44789a4ad
wuziyue2009 <2184478323@qq.com>
2026-09-05 14:56:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ const localBindingConfig = {
     ? [
         {
           binding: d1,
-          database_name: 'site-creator-d1',
+          database_name: 'doudizhu',
           database_id: SITE_CREATOR_PLACEHOLDER_DATABASE_ID,
         },
       ]
