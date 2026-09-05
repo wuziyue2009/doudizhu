6513f0fa4826f12ad72c11838ad1f2571613870d
wuziyue2009 <2184478323@qq.com>
2026-09-05 14:56:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import hostingConfig from './.openai/hosting.json';
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  '00000000-0000-4000-8000-000000000000';
+  '59d7eeb2-bfb0-4b0f-a01c-5b5c54a459ae';
 
 const { d1, r2 } = hostingConfig;
 
